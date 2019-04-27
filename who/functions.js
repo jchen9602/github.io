3c9fb7185eef5dfe012734d614ee131373f5422d
jchen <1443616037@qq.com>
2019-04-27 13:22:37 +0800

    change music and add time

--- who/functions.js
+++ who/functions.js
@@ -38,13 +38,15 @@ $(window).resize(function() {
 })(jQuery);
 
 function timeElapse(date){
-	var current = Date();
+	var t1="2019/04/17 23:33:52"
+	date=new Date(t1)
+	var current = new Date();
 	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
 	var days = Math.floor(seconds / (3600 * 24));
 	seconds = seconds % (3600 * 24);
 	var hours = Math.floor(seconds / 3600);
 	if (hours < 10) {
-		hours = "0" + hours;
+		hours = "0" + hours;	
 	}
 	seconds = seconds % 3600;
 	var minutes = Math.floor(seconds / 60);
@@ -56,5 +58,5 @@ function timeElapse(date){
 		seconds = "0" + seconds;
 	}
 	var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒"; 
-	$("#clock").html(result);
+	$("#clock").html(result);	
 }
